3f46df1f80c495f380bf6776e8054ac5e6e3ff85
Joel Roggeman <joelroggeman@gmail.com>
2016-05-16 10:18:57 -0400

    Add docs to ST public methods

--- src/com/symbol_table/SymbolTable.java
+++ src/com/symbol_table/SymbolTable.java
@@ -11,14 +11,30 @@ public class SymbolTable {
         currentScope = new Scope();
     }
 
+    /**
+     * Adds a new entry to the symbol table in the current scope
+     * @param key The Identifier of the entry to add
+     * @param value The actual Entry object to add
+     */
     public void put(Identifier key, Entry value) {
         currentScope.put(key, value);
     }
 
+    /**
+     * Retrieves an entry from the symbol table.  We start by looking in the current scope,
+     * and then cascade up the parent scopes.
+     * @param key The Identifier that we're looking for.
+     * @return The associated Entry object.
+     */
     public Entry get(Identifier key) {
         return currentScope.get(key);
     }
 
+    /**
+     * Enter's a new scope identified by scopeIdentifier.
+     * @param scopeIdentifier The object representing the parent of the entire scope.  For example, for a function,
+     *                        this would be the Function object, but for an If block, this would be the If object.
+     */
     public void enterScope(Object scopeIdentifier) {
         if(!currentScope.childScopes.containsKey(scopeIdentifier)) {
             Scope newScope = new Scope(currentScope);
@@ -28,6 +44,10 @@ public class SymbolTable {
         currentScope = currentScope.childScopes.get(scopeIdentifier);
     }
 
+    /**
+     * Leave the current scope, going up one level.
+     * @throws RuntimeException if we're already in the topmost scope.
+     */
     public void leaveScope() {
         if(currentScope.parent == null) {
             throw new RuntimeException("Can't leave the parent scope");
