255edc6164d662e8bdcf1c63e3fc52d001a30138
Joel Roggeman <joelroggeman@gmail.com>
2016-05-20 13:09:10 -0400

    Add unary to visitor interface

--- src/com/visitors/Visitor.java
+++ src/com/visitors/Visitor.java
@@ -5,7 +5,8 @@ import com.ast.Program;
 import com.ast.expressions.Call;
 import com.ast.expressions.Subexpression;
 import com.ast.expressions.binary.*;
-import com.ast.expressions.literals.Literal;
+import com.ast.expressions.literals.*;
+import com.ast.expressions.unary.Not;
 import com.ast.expressions.unary.UnaryExpression;
 import com.ast.function.Function;
 import com.ast.function.ParamDeclaration;
@@ -13,6 +14,7 @@ import com.ast.function.ParamDeclarationList;
 import com.ast.mutable.Identifier;
 import com.ast.mutable.Mutable;
 import com.ast.statements.*;
+import com.ast.types.Type;
 import com.ast.types.TypeDeclaration;
 import com.exceptions.TypeCheckException;
 
@@ -38,16 +40,25 @@ public interface Visitor {
     void visit(While element) throws TypeCheckException;
     void visit(TypeDeclaration element) throws TypeCheckException;
 
-    /* Binary expressions */
-    void visit(Add expression) throws TypeCheckException;
-    void visit(And expression) throws TypeCheckException;
-    void visit(Divide expression) throws TypeCheckException;
-    void visit(Equals expression) throws TypeCheckException;
-    void visit(LessThan expression) throws TypeCheckException;
-    void visit(LessThanOrEquals expression) throws TypeCheckException;
-    void visit(Modulo expression) throws TypeCheckException;
-    void visit(Multiply expression) throws TypeCheckException;
-    void visit(NotEquals expression) throws TypeCheckException;
-    void visit(Or expression) throws TypeCheckException;
-    void visit(Subtract expression) throws TypeCheckException;
+    /* Binary elements */
+    void visit(Add element) throws TypeCheckException;
+    void visit(And element) throws TypeCheckException;
+    void visit(Divide element) throws TypeCheckException;
+    void visit(Equals element) throws TypeCheckException;
+    void visit(LessThan element) throws TypeCheckException;
+    void visit(LessThanOrEquals element) throws TypeCheckException;
+    void visit(Modulo element) throws TypeCheckException;
+    void visit(Multiply element) throws TypeCheckException;
+    void visit(NotEquals element) throws TypeCheckException;
+    void visit(Or element) throws TypeCheckException;
+    void visit(Subtract element) throws TypeCheckException;
+
+    /* Literal Expressions */
+    void visit(BoolLiteral element) throws TypeCheckException;
+    void visit(CharLiteral element) throws TypeCheckException;
+    void visit(DoubleLiteral element) throws TypeCheckException;
+    void visit(IntLiteral element) throws TypeCheckException;
+
+    /* Unary Expressions */
+    void visit(Not element) throws TypeCheckException;
 }
