ecec95c5e0865b0855983cad04665a3558b22f51
Joel Roggeman <joelroggeman@gmail.com>
2016-05-20 13:06:29 -0400

    Add specific binary operators back into visitor interface

--- src/com/visitors/Visitor.java
+++ src/com/visitors/Visitor.java
@@ -4,7 +4,7 @@ import com.ast.Block;
 import com.ast.Program;
 import com.ast.expressions.Call;
 import com.ast.expressions.Subexpression;
-import com.ast.expressions.binary.BinaryExpression;
+import com.ast.expressions.binary.*;
 import com.ast.expressions.literals.Literal;
 import com.ast.expressions.unary.UnaryExpression;
 import com.ast.function.Function;
@@ -37,4 +37,17 @@ public interface Visitor {
     void visit(VariableDeclaration element) throws TypeCheckException;
     void visit(While element) throws TypeCheckException;
     void visit(TypeDeclaration element) throws TypeCheckException;
+
+    /* Binary expressions */
+    void visit(Add expression) throws TypeCheckException;
+    void visit(And expression) throws TypeCheckException;
+    void visit(Divide expression) throws TypeCheckException;
+    void visit(Equals expression) throws TypeCheckException;
+    void visit(LessThan expression) throws TypeCheckException;
+    void visit(LessThanOrEquals expression) throws TypeCheckException;
+    void visit(Modulo expression) throws TypeCheckException;
+    void visit(Multiply expression) throws TypeCheckException;
+    void visit(NotEquals expression) throws TypeCheckException;
+    void visit(Or expression) throws TypeCheckException;
+    void visit(Subtract expression) throws TypeCheckException;
 }
